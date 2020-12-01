88433d5cfb611665922fa0b8f528cbc77dcbf2d4
Michael Stoneman <michaelstoneman@u.boisestate.edu>
2020-11-30 21:09:00 -0700

    removing obsolete files

--- MarshallPews/footer.php
+++
@@ -1,9 +0,0 @@
-	<div class ="footer">
-		<p> This is the footer | I'll fill this with relevent info and a hyperlink or two </p>
-		<a href="logout.php" id="logout"> Logout </a>
-	</div>
-
-	<?php if(!empty($_SESSION['authenticated'])){ if($_SESSION['authenticated']){ ?>
-	<script type="text/javascript">document.getElementById('logout').style.display = 'inline';</script>
-	<?php } } ?>
-
